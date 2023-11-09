a1d2a0db8fe11e5adb337cd350e24e73257cd138
Matthias Lee <matthias@matthiasclee.com>
2023-11-08 19:07:09 -0800

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Packages
+
+sudo apt update
+sudo apt install libboost-all-dev libcups2 libcups2-dev libcubsimage2-dev automake-1.16
+
+# Unzip
+cat 5xx-drivers.zip.b64 | base64 -d > 5xx-drivers.zip
+unzip 5xx-drivers.zip
