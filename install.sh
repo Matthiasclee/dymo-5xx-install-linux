27a9ad9be2cd9ae35273e1abb0bae7423923b52a
Matthias Lee <matthias@matthiasclee.com>
2023-11-08 19:18:36 -0800

    Misc

--- install.sh
+++ install.sh
@@ -1,8 +1,44 @@
 # Packages
 
 sudo apt update
-sudo apt install libboost-all-dev libcups2 libcups2-dev libcubsimage2-dev automake-1.16
+sudo apt install libboost-all-dev libcups2 libcups2-dev libcupsimage2-dev automake-1.16
 
 # Unzip
 cat 5xx-drivers.zip.b64 | base64 -d > 5xx-drivers.zip
 unzip 5xx-drivers.zip
+
+cd LW5xx\ Linux\ 1.4.3
+
+# Configure and make
+autoreconf -f -i
+./configure
+confexit=$?
+if [ "$confexit" != "0" ]; then
+  echo -e "\e[31m./configure exited with code $confexit.\e[0m"
+  exit 1
+fi
+make
+makeexit=$?
+if [ "$makeexit" != "0" ]; then
+  echo -e "\e[31mmake exited with code $makeexit.\e[0m"
+  exit 1
+fi
+
+# Install
+sudo make install
+makeinstallexit=$?
+if [ "$makeinstallexit" != "0" ]; then
+  echo -e "\e[31msudo make install exited with code $makeinstallexit.\e[0m"
+  exit 1
+fi
+
+cd ..
+
+# Clean up
+rm 5xx-drivers.zip
+rm -r LW5xx\ Linux\ 1.4.3
+
+# Alert
+echo -e '\e[92mInstallation done! You can add the printer through the printers interface.\e[0m'
+zenity --info --title "Dymo Driver Installer" --text "Installation Done\!\n\nYou can now add the Dymo label printer\nthrough the printers interface." --no-wrap & 
+
